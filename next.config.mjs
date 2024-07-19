ebcb8a5b1b5339b6a43ec24c1ef1dda2e78f6682
John R. D'Orazio <priest@johnromanodorazio.com>
2024-07-19 12:20:38 +0200

    This commit corrects the mistake

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,8 @@ const nextConfig = {
   distDir: './dist', // Changes the build output directory to `./dist/`.
   images: {
     unoptimized: true // Image optimization cannot be used with output: 'export'
-  }
+  },
+  basePath: process.env.NEXT_PUBLIC_URL === '/' ? '' : process.env.NEXT_PUBLIC_URL
 };
 
 export default nextConfig;
